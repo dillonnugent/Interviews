4e8c08cb230cdfa00e2570e9586800a7ca4e8516
Dillon Nugent <dillonn@microsoft.com>
2017-05-17 17:53:18 -0700

    Added string manipulation functions

--- Interviews/Program.cs
+++ Interviews/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace Interviews
 {
-    class Program
+    class StringRoutines
     {
         static void Main(string[] args)
         {
+            String test = removeChars("Battle of the Vowels: Hawaii vs. Gronzy", "aeiou");
+            Console.WriteLine(test);
+            Console.ReadLine();
+        }
+
+        static String removeChars(String str, String remove)
+        {
+            char[] s = str.ToCharArray();
+            char[] r = remove.ToCharArray();
+            int src, dst = 0;
+
+            // flags automatically initialized to false, size of 128 assumens ASCII
+            Boolean[] flags = new Boolean[128];
+
+            // Set flags for characters to be removed
+            for(src = 0; src < r.Length; ++src)
+            {
+                flags[r[src]] = true;
+            }
+
+            // Now loop through all the characters, copying only if they aren't flagged
+            for(src = 0; src < s.Length; ++src)
+            {
+                if(!flags [s[src]])
+                {
+                    s[dst++] = s[src];
+                }
+            }
+
+            return new String(s, 0, dst);
         }
     }
 }
