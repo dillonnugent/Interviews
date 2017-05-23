3844d1837d07ff927c6c789addff2e7c4555b0fd
Dillon Nugent <dillonn@microsoft.com>
2017-05-23 12:52:52 -0700

    Reorganized and added WordCount

--- Interviews/Program.cs
+++ Interviews/Program.cs
@@ -6,40 +6,19 @@ using System.Threading.Tasks;
 
 namespace Interviews
 {
-    class StringRoutines
+    class RunAtConsole
     {
+        public const String testString = "";
+
         static void Main(string[] args)
         {
-            String test = removeChars("Battle of the Vowels: Hawaii vs. Gronzy", "aeiou");
-            Console.WriteLine(test);
+            String testRemoveChars = StringManipulation.RemoveChars(testString, "aeiou");
+            Console.WriteLine(testRemoveChars);
+
+            int testWordCount = StringManipulation.WordCount(testString);
+            Console.WriteLine(testWordCount);
+
             Console.ReadLine();
-        }
-
-        static String removeChars(String str, String remove)
-        {
-            char[] s = str.ToCharArray();
-            char[] r = remove.ToCharArray();
-            int src, dst = 0;
-
-            // flags automatically initialized to false, size of 128 assumens ASCII
-            Boolean[] flags = new Boolean[128];
-
-            // Set flags for characters to be removed
-            for(src = 0; src < r.Length; ++src)
-            {
-                flags[r[src]] = true;
-            }
-
-            // Now loop through all the characters, copying only if they aren't flagged
-            for(src = 0; src < s.Length; ++src)
-            {
-                if(!flags [s[src]])
-                {
-                    s[dst++] = s[src];
-                }
-            }
-
-            return new String(s, 0, dst);
         }
     }
 }
---
+++ Interviews/StringManipulation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interviews
+{
+    class StringManipulation
+    {
+        /// <summary>
+        /// Arrays - Removes one string from another in place
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="remove"></param>
+        /// <returns></returns>
+        public static String RemoveChars(String str, String remove)
+        {
+            if (str == null)
+            {
+                return "Exception: null or empty string";
+            }
+
+            char[] s = str.ToCharArray();
+            char[] r = remove.ToCharArray();
+            int src, dst = 0;
+
+            // flags automatically initialized to false, size of 128 assumens ASCII
+            Boolean[] flags = new Boolean[128];
+
+            // Set flags for characters to be removed
+            for (src = 0; src < r.Length; ++src)
+            {
+                flags[r[src]] = true;
+            }
+
+            // Now loop through all the characters, copying only if they aren't flagged
+            for (src = 0; src < s.Length; ++src)
+            {
+                if (!flags[s[src]])
+                {
+                    s[dst++] = s[src];
+                }
+            }
+
+            return new String(s, 0, dst);
+        }
+
+        public static int WordCount(String str)
+        {
+            if(str == null) {return 0;}
+            str = str.Trim();
+            int wordCount = 0, index =0;
+            while (index < str.Length)
+            {
+                // check if the current character is part of a word
+                while (index < str.Length && !char.IsWhiteSpace(str[index]))
+                {
+                    index++;
+                }
+                
+                wordCount++;
+                
+                // skip whitespace until next word
+                while (index < str.Length && char.IsWhiteSpace(str[index]))
+                {
+                    index++;
+                }
+            }
+            return wordCount;
+        }
+
+
+    }
+}
